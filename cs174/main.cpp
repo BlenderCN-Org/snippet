80652d16a6dc192066437123e8078d51d8821552
qatnonoil <ozawa@polyphony.co.jp>
2017-11-04 18:22:49 +0900

    Markov Boundsの追加

--- cs174/main.cpp
+++ cs174/main.cpp
@@ -10,6 +10,7 @@
 #include <cstdio>
 #include <cstdint>
 #include <random>
+#include <array>
 
 //
 class FloatStreamStats
@@ -47,32 +48,50 @@ protected:
     float max_ = std::numeric_limits<float>::min();
 };
 
-//
+/*
+ Markov bound
+ Pr[Y≥t] ≤ E[Y] / t
+ の確認
+ */
 void lecture6_markov_bound()
 {
-    // TODO: Markov bound
     //
     std::random_device rd;
     std::mt19937 rng(rd());
     FloatStreamStats fs;
     //
-    std::normal_distribution<> dist(0.0f, 1.0f);
-    for(int32_t i=0;i<512;++i)
+    const float mu = 5.0f;
+    const float sd = 1.0f;
+    std::array<int,10> Count; // Pr[Y≥t]となったカウント
+    std::fill(Count.begin(),Count.end(),0);
+    std::normal_distribution<> dist(mu, sd);
+    const int32_t numSample = 512;
+    for(int32_t i=0;i<numSample;++i)
     {
         // 非負はいけないことに注意
-        const float v = std::min(dist(rng), 0.0);
+        const float v = std::max(dist(rng), 0.0);
         fs.add(v);
+        for(int32_t j=1;j<Count.size()+1;++j)
+        {
+            if(float(j) < v)
+            {
+                ++Count[j];
+            }
+        }
     }
-    
-    // TODO: なんらかの分布に従うように生成する
-    printf("DONE¥n");
+    //
+    printf("E[X]=%f\n", fs.mean());
+    printf("        Pr[Y≥t] ≤ E[Y]/t \n");
+    for(int32_t i=0;i<10;++i)
+    {
+        const float t = (mu)/10.0f * float(i);
+        const float Pr = float(Count[i])/float(numSample);
+        printf("t:%3.3f  %3.3f  ≤  %3.3f\n", t, Pr, fs.mean()/t);
+    }
 }
 
 int main()
 {
-    printf("fff\n");
-    puts("FUCK");
-    printf("ggg\n");
-    fflush(stdout);
+    lecture6_markov_bound();
     return 0;
 }
