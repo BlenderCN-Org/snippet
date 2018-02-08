3cf4901aad2bceb0473c38c9e483888b103c4d0a
Yuki Ozawa <q.at.nonoil@gmail.com>
2018-02-08 13:25:19 +0900

    update

--- optimization_quadratic/main.cpp
+++ optimization_quadratic/main.cpp
@@ -22,12 +22,90 @@
  - ペナルティ法もパラメーター自体に制限がある...? がどうやってサンプルにないところをペナルティとして指定するのか？
  */
 
+#include <Eigen/Eigenvalues>
+#include <iostream>
+
+#include "eigen-qp.hpp"
+
+using namespace std;
+using namespace Eigen;
+using namespace EigenQP;
+/*
+ *  min 0.5 x.Q.x + c.x
+ *
+ *  Ax <= b
+ *  Ex = d
+ *
+ * See: http://etd.dtu.dk/thesis/220437/ep08_19.pdf
+ */
+
+#define PROB_VARS 16
+#define PROB_INEQ 8
+#define PROB_EQ 3
+
+template<typename MatrixT>
+void checkPosDef(MatrixT& A)
+{
+    //Eigen::MatrixXd A(2, 2);
+    //A << 1, 0 , 0, -1; // non semi-positive definitie matrix
+    //std::cout << "The matrix A is" << std::endl << A << std::endl;
+    Eigen::LLT<Eigen::MatrixXd> lltOfA(A); // compute the Cholesky decomposition of A
+    const bool isPosDef = lltOfA.info() != Eigen::NumericalIssue;
+    auto mes = isPosDef ? "Positive" : "Negative";
+    std::cout << mes << std::endl;
+}
+
+template<typename Scalar, int NV_FIXED, int NC_FIXED, int NE_FIXED>
+void test()
+{
+    
+    
+    typedef Eigen::Matrix<Scalar,-1,-1> MatrixXs;
+    typedef Eigen::Matrix<Scalar,-1,1> VectorXs;
+    
+    // Make a random problem
+    int num_vars = NV_FIXED;
+    int num_ineq = NC_FIXED;
+    
+    // Random matrices
+    MatrixXs Q = MatrixXs::Random(num_vars,num_vars);
+    Q *= Q.adjoint()/sqrt(num_vars); // Make it pos def
+    
+    //checkPosDef(Q);
+    
+    VectorXs c = VectorXs::Random(num_vars);
+    
+    MatrixXs A = MatrixXs::Random(num_ineq,num_vars);
+    VectorXs b = VectorXs::Random(num_ineq);
+    
+    // Solve unconstrainted system
+    cout << "Unconstrained..." << endl;
+    VectorXs x_unc = -Q.ldlt().solve(c);
+    // これはほぼ０
+    //cout << (Q * -x_unc - c).norm() << endl;
+    
+    std::cout << x_unc << std::endl;
+    VectorXs x(num_vars);
+    // Generate inequality constraints
+    b.array() = (A*x_unc).array() - 0.15;
+    cout << "constraint..." << endl;
+    std::cout << b << std::endl;
+    // Inequality constrained problem
+    cout << "quadprog, dynamic code" << endl;
+    quadprog(Q,c,A,b,x);
+    std::cout << x << std::endl;
+    cout << "    error: " << (x - x_unc).norm() << endl;
+}
+
 /*
  二次方程式も解けることの確認
  */
 int32_t main()
 {
-        //test<double,PROB_VARS,PROB_INEQ,PROB_EQ>();
+#if 0
+    test<double,PROB_VARS,PROB_INEQ,PROB_EQ>();
+    return 0;
+#endif
     
     //return 0;
     struct Sample
@@ -116,8 +194,9 @@ int32_t main()
         A2.resize(t,t);
         b2.resize(t);
         A2.setIdentity();
-        b2.setOnes();
-        b2 << 1.0/10.0, 1.0/10.0, 1.0/10.0;
+        b2(0)  = 1/3.0f;
+        b2(1)  = 1/3.0f;
+        b2(2)  = 1/3.0f;
         //
         Eigen::VectorXd x;
         x.resize(t);
@@ -126,6 +205,8 @@ int32_t main()
         //
         std::cout << "Rest" << std::endl;
         std::cout << A2 * x  << std::endl;
+        
+        //checkPosDef(Q);
 #if 0
         template<typename Scalar, int NVars, int NIneq>
         void quadprog(Eigen::Matrix<Scalar,NVars,NVars> &Q, Eigen::Matrix<Scalar,NVars,1> &c,
